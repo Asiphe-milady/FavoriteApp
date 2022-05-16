3dbdd53090bfff1ddceb62d5bcb1a792bb638b6e
ASIPHE NCOMBO <4270171@myuwc.ac.za>
2022-05-16 15:30:18 +0200

    first attempt

---
+++ favoriteApp/favoriteApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace favoriteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public static NavigationPage FavApp { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            //NewMethod(navigationPage);
+
+        }
+
+        private void NewMethod(NavigationPage navigationPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnEpisodesSwipeItemInvoked(object sender, EventArgs a)
+        {
+
+        }
+    }
+}
